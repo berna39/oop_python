6a21d73cacddb1ecc90e77277fe460394d17c0b8
berna39 <kalemajoseph39@gmail.com>
2022-01-09 23:55:58 +0200

    Initializing data

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
 class Calculator:
     def __init__(self, number_one, number_two, operand):
-        
+        self.number_one = number_one
+        self.number_two = number_two
+        self.operand = operand
