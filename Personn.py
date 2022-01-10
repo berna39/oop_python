e00006cc5c184db6efc1764f40ce58e2d95bf996
berna39 <kalemajoseph39@gmail.com>
2022-01-10 14:56:20 +0200

    Adding from file constructor

--- Personn.py
+++ Personn.py
@@ -21,6 +21,13 @@ class Person:
         data = person_as_string.split('-')
         return cls(data[0], data[1], data[2])
 
+    #a contructor from file
+    @classmethod
+    def from_file(cls, file):
+        with open(file) as file:
+            data = file.readline().strip().split('-')
+            return cls(data[0], data[1], data[2])
+
 
 #p = Person('Shango jos', 'm', 'Kenya')
 #p.introduce()
@@ -33,4 +40,7 @@ print(person_fr_str)
 with open('people.txt') as file:
     person = Person.from_string(file.readline().strip())
     print(person)
+
+other_person = Person.from_file('other_one.txt')
+print(other_person)
     
