6bec5a83c9f5128b2eab7f3274b6f7425736b908
berna39 <kalemajoseph39@gmail.com>
2022-01-12 21:37:35 +0200

    Refractored calcutator class

--- calculator.py
+++ calculator.py
@@ -1,25 +1,26 @@
 class Calculator:
     def __init__(self, number_one, number_two, operand):
         pass
-    def start():
-        first_name = input('enter the first number : \n')
+    def addition(first_number, second_number): #for addition operation
+        return first_number + second_number
+
+    def substraction(first_number, second_number):
+        return first_number - second_number
+
+    def multiply(first_number, second_number):
+        return first_number * second_number
+
+    def divide(first_number, second_number):
+        return first_number / second_number
+
+    @staticmethod
+    def start(self):
+        first_number = input('enter the first number : \n')
         second_number = input('enter the second number : \n')
-        
+        operation = input("choose between these operations : 'add', 'sub', 'div', 'exp'")
 
-    def addition():
-        pass
-    def substraction():
-        pass
-    def multiply():
-        pass
-    def divide():
-        pass
-    def modulo():
-        pass
-    def exp():
-        pass
-    def print_result():
-        pass
-    def end():
-        pass
+        if operation == 'add':
+            print(self.ddition(first_number, second_number))
 
+
+Calculator.start()
