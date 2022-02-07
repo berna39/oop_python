d31419e45aae2a36339574cdbdd153cdb05abf73
berna39 <kalemajoseph39@gmail.com>
2022-02-07 22:57:18 +0200

    More methons to Cadidate class

--- votes.py
+++ votes.py
@@ -1,4 +1,8 @@
 class Candidate:
-    def __init__(self):
-        pass
-    
+    def __init__(self, name, number, group):
+        self.name = name
+        self.number = number
+        self.group = group
+
+    def __repr__(self) -> str:
+        return f'i\'m {self.name} the number {self.number} of the group {self.group}'
